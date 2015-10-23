4660733e05880597a08e8d4271c0899f2a9a6c8e
zankowitch alexis <zankowitch@gmail.com>
2015-10-23 09:29:30 +0200

    fixexs import

--- TP-Jee/src/java/dao/DAO.java
+++ TP-Jee/src/java/dao/DAO.java
@@ -5,10 +5,33 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author azank
  */
-public class DAO {
-    
+public abstract class DAO<T, T1> {
+
+    public Connection connect = null;
+    T metier;
+
+    public DAO() {
+        try {
+            this.connect = DBManager.getConnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract boolean create(T obj);
+
+    public abstract boolean delete(T obj);
+
+    public abstract boolean update(T obj);
+
+    public abstract T find(T1 id);
 }
--- TP-Jee/src/java/dao/DBManager.java
+++ TP-Jee/src/java/dao/DBManager.java
@@ -5,10 +5,41 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author azank
  */
 public class DBManager {
-    
+
+    private static Connection connect;
+
+    private DBManager() {
+    }
+
+    private static void initConnexion(String driver, String url, String login, String password) throws SQLException {
+        try {
+            Class.forName(driver);
+            connect = DriverManager.getConnection(url, login, password);
+            connect.setAutoCommit(false);
+            System.out.println("Connexion reussie");
+        } catch (ClassNotFoundException e) {
+            System.out.println("erreur chargement pilote JDBC \n");
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println("erreur connexion base de données \n" + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static Connection getConnect() throws SQLException {
+        if (!(connect instanceof Connection)) {
+            initConnexion("org.apache.derby.jdbc.ClientDriver", "jdbc:derby://localhost:1527/test", "test", "test");
+        }
+        return connect;
+    }
 }
+
