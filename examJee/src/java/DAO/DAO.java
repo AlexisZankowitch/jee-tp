837b51d7004c9768aea49434400b3b9aed46e84d
zankowitch alexis <zankowitch@gmail.com>
2015-10-23 13:55:42 +0200

    DAO + bean

---
+++ examJee/src/java/DAO/DAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author azank
+ */
+public abstract class DAO<T, T1> {
+
+    public Connection connect = null;
+    T metier;
+
+    public DAO() {
+        try {
+            this.connect = DBManager.getConnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract boolean create(T obj);
+
+    public abstract boolean delete(T obj);
+
+    public abstract boolean update(T obj);
+
+    public abstract T findById(T1 id);
+    
+    public abstract List<T> findBySsn(T1 id);
+}
---
+++ examJee/src/java/beans/User.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author azank
+ */
+public class User {
+    private String firstname;    
+    private String birthname;    
+    private String login;  
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getBirthname() {
+        return birthname;
+    }
+
+    public void setBirthname(String birthname) {
+        this.birthname = birthname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    
+}
